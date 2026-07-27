2da31be2dd44952d3e567f22591fbc61b3a00806
antoinedelagrave <antoinedelagrave@hotmail.com>
2026-07-27 09:22:14 -0400

    (doc) Documented "__init__.py"

--- bath_parametrizer/__init__.py
+++ bath_parametrizer/__init__.py
@@ -5,4 +5,6 @@ table and generators of an abelian point group (Pyqcm formalism).
 from .bath_parametrization import BathParametrizer
 from .point_groups import all_point_groups
 
+# Defines what modules/submodules are imported
+# when the wildcard import * is used!
 __all__  = ["BathParametrizer", "all_point_groups"]
